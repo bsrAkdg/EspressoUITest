1b9829b9fce93fead27c61e2e3db4ab6a5446b3d
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-24 19:54:55 +0300

    Activity navigation testing

--- Movies/app/src/androidTest/java/com/bsrakdg/movies/MainActivityTest.kt
+++ Movies/app/src/androidTest/java/com/bsrakdg/movies/MainActivityTest.kt
@@ -2,41 +2,38 @@ package com.bsrakdg.movies
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
-
     @Test
-    fun test_isActivityInView() {
+    fun test_navSecondaryActivity() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
-        onView(withId(R.id.main)).check(matches(isDisplayed())) // layout parent view id
+        onView(withId(R.id.button_next_activity)).perform(click()) // trigger start secondary activity start
+
+        onView(withId(R.id.secondary)).check(matches(isDisplayed())) // is secondary exist ??
     }
 
     @Test
-    fun test_visibility_title_nextButton() {
+    fun test_backPress_toMainActivity() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
-        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed())) // is title exist??
+        onView(withId(R.id.button_next_activity)).perform(click()) // trigger start secondary activity start
 
-        onView(withId(R.id.button_next_activity)).check(matches(isDisplayed())) // is next button exist??
+        onView(withId(R.id.secondary)).check(matches(isDisplayed())) // is secondary exist ??
 
-        onView(withId(R.id.button_next_activity))
-            .check(matches(withEffectiveVisibility(Visibility.VISIBLE))) // is next button visible??
+        onView(withId(R.id.button_back)).perform(click()) // trigger back button // method 1
 
-    }
+        // pressBack() // method 2
 
-    @Test
-    fun test_isTitleTextDisplayed() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.activity_main_title))
-            .check(matches(withText(R.string.text_main_activity))) // is title correct ??
-
+        onView(withId(R.id.main)).check(matches(isDisplayed())) // is main exist ??
     }
 }
