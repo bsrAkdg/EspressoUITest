5a3a5291763cb8159d6d03e940fe5ba9053f0878
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-25 12:02:14 +0300

    Fragments in Isolation

---
+++ Movies/app/src/androidTest/java/com/bsrakdg/movies/ui/movie/StarActorsFragmentTest.kt
@@ -0,0 +1,46 @@
+package com.bsrakdg.movies.ui.movie
+
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.bsrakdg.movies.R
+import com.bsrakdg.movies.factory.MovieFragmentFactory
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class StarActorsFragmentTest {
+    @Test
+    fun test_isActorsListVisible() {
+        // SETUP
+        val actors = arrayListOf(
+            "Dwayne Johnson",
+            "Seann William Scott",
+            "Rosario Dawson",
+            "Christopher Walken"
+        )
+        val fragmentFactory = MovieFragmentFactory()
+        val bundle = Bundle()
+        bundle.putStringArrayList("args_actors", actors)
+
+        val scenario = launchFragmentInContainer<StarActorsFragment>(
+            fragmentArgs = bundle,
+            factory = fragmentFactory
+        )
+
+        // VERIFY
+        onView(withId(R.id.star_actors_text)).check(
+            matches(
+                withText(
+                    StarActorsFragment.stringBuilderForStarActors( // expected value
+                        actors
+                    )
+                )
+            )
+        )
+    }
+}
