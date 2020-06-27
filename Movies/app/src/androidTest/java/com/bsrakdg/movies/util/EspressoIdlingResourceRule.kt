1bd9b51110d6c224517f87b8c2590422dc30a863
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-27 23:02:05 +0300

    Custom rule option 2 (simple way)

--- Movies/app/src/androidTest/java/com/bsrakdg/movies/util/EspressoIdlingResourceRule.kt
+++ Movies/app/src/androidTest/java/com/bsrakdg/movies/util/EspressoIdlingResourceRule.kt
@@ -1,35 +1,51 @@
 package com.bsrakdg.movies.util
 
 import androidx.test.espresso.IdlingRegistry
-import org.junit.rules.TestRule
+import org.junit.rules.TestWatcher
 import org.junit.runner.Description
-import org.junit.runners.model.Statement
 
 /*
     Option 1 (the long way) :
     This option is much more difficult to read and is more verbose.
  */
-class EspressoIdlingResourceRule : TestRule {
+// class EspressoIdlingResourceRule : TestRule {
+//    override fun apply(base: Statement?, description: Description?): Statement {
+//        return IdlingResourceStatement(base)
+//    }
+//
+//    class IdlingResourceStatement(private val base: Statement?) : Statement() {
+//        private val idlingResource = EspressoIdlingResource.countingIdlingResource
+//
+//        override fun evaluate() {
+//            // @Before
+//            IdlingRegistry.getInstance().register(idlingResource)
+//            try {
+//                base?.evaluate()
+//                    ?: throw Exception("Error evaluating test. Base statement is null.")
+//            } finally {
+//                // @After
+//                IdlingRegistry.getInstance().unregister(idlingResource)
+//            }
+//        }
+// }
+// }
 
-    override fun apply(base: Statement?, description: Description?): Statement {
-        return IdlingResourceStatement(base)
-    }
-
-    class IdlingResourceStatement(private val base: Statement?) : Statement() {
+/*
+    Option 2 (the simple way) :
+    Simplified version of option#1. (TextWatcher class implements TestRule)
+*/
+class EspressoIdlingResourceRule : TestWatcher() {
         private val idlingResource = EspressoIdlingResource.countingIdlingResource
 
-        override fun evaluate() {
-            // @Before
-            IdlingRegistry.getInstance().register(idlingResource)
-            try {
-                base?.evaluate()
-                    ?: throw Exception("Error evaluating test. Base statement is null.")
-            } finally {
-                // @After
-                IdlingRegistry.getInstance().unregister(idlingResource)
-            }
+    // @After
+    override fun finished(description: Description?) {
+        IdlingRegistry.getInstance().unregister(idlingResource)
+        super.finished(description)
         }
 
-
+    // @Before
+    override fun starting(description: Description?) {
+        IdlingRegistry.getInstance().register(idlingResource)
+        super.starting(description)
     }
 }
